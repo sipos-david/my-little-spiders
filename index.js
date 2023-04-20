14492addb3a52c4f0cf7fe5cf2206410e91b7782
Sipos Áron Dávid <sipisipos@gmail.com>
2023-04-20 16:10:55 +0200

    Add ejs views

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
+app.set('view engine', 'ejs');
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use('/public', express.static("public"));
 
 require('./routes/index')(app);
