9996fde9dc20254baa0e16f18c55630ec3d2e4e5
Sipos Áron Dávid <sipisipos@gmail.com>
2023-04-28 20:04:02 +0200

    Removed unused import

--- middleware/roommate/getRoommates.mw.js
+++ middleware/roommate/getRoommates.mw.js
@@ -2,7 +2,6 @@
  * Load all roommates from the database
  * The result is saved to res.locals.roommates
  */
-const Roommate = require("../../models/roommate");
 module.exports = function (objRepo) {
     return function (req, res, next) {
         return objRepo.db.Roommate.find({}, function (err, roommates) {
